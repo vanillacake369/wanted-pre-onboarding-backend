ecee499367c928e92b04d7ca574b6f849a3742f6
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-08-04 18:48:43 +0900

    feat : email validation

--- src/main/java/com/wanted/onboarding/entity/User.java
+++ src/main/java/com/wanted/onboarding/entity/User.java
@@ -1,7 +1,10 @@
 package com.wanted.onboarding.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
+
+import java.util.regex.Pattern;
 
 @Getter // getter 메소드 생성
 @Builder // 빌더를 사용할 수 있게 함
@@ -16,6 +19,7 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     private String email;
 
     @Column(nullable = false)
--- src/test/java/com/wanted/onboarding/repo/UserRepositoryTest.java
+++ src/test/java/com/wanted/onboarding/repo/UserRepositoryTest.java
@@ -1,4 +1,34 @@
 package com.wanted.onboarding.repo;
 
+import com.wanted.onboarding.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("mysql_test")
 public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("Email Validation")
+    public void testEmailValidation() throws Exception{
+        /* GIVEN */
+        User unvalid = User.builder().email("worng email").password("random pw").build();
+
+        /* THEN :: EXPECTED EXCEPTION */
+        Exception exception = Assertions.assertThrows(Exception.class, () -> {
+            /* WHEN */
+            userRepository.save(unvalid);
+        });
+
+        /* THEN :: EXPECTED EXCEPTION MESSAGES */
+        org.assertj.core.api.Assertions.assertThat(exception).isInstanceOf(Exception.class);
+        System.out.println(exception.getMessage());
+
+    }
 }
